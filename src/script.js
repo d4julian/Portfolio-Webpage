55c904f41bb2ec1fc8038e7970f34c70e66768ed
d4julian <47072615+d4julian@users.noreply.github.com>
2024-07-18 17:45:44 -0400

    Finish projects section

--- src/script.js
+++ src/script.js
@@ -2,13 +2,17 @@ import showdown from "showdown";
 
 const home = `My name is Julian Ubico. I'm a software developer currently pursuing a Bachelor of Science in Computer Science (Spring 2025) at the University of Florida. I have a diverse skill set in programming languages like Java, SQL, HTML, CSS, JavaScript, and C++, and I am proficient in Linux system administration. I specialize in creating responsive, mobile-friendly websites, setting up web servers with NGINX, and developing full stack applications. I first learned how to program at the age of 16, with Java being my foundation. My most notable project is [DirtCraft](https://dirtcraft.net/), a modded minecraft network which I co-founded. During the summer of 2021, I also completed an internship with [Tech For Good](https://www.techforgoodinc.org/), a not-for-profit organization focused on providing access to technology for the less fortunate. I collaborated with team members on projects, actively participated in the daily meetings, and gained experience in a real-world software engineering environment.`;
 const skills = `I am experienced with databases such as H2, MySQL, and MariaDB. I have strong skills in Linux, particularly CentOS 7, and am proficient in system administration. I have utilized Jenkins for automated build processes. I am adept at setting up and managing web servers using NGINX. I can develop responsive, mobile-friendly websites and full stack applications. Additionally, I am well-versed in software engineering practices, including version control with Git, continuous integration and delivery (CI/CD), and agile methodologies.<br><br>**Programming Languages**`;
-const projects = `
-#### [Dirt Panel](https://github.com/d4julian)
+const projects = 
+`####[Dirt Panel](https://github.com/d4julian/Dirt-Panel)
 Developed a full stack administration panel with a React frontend and an Express & Java backend. The panel uses MySQL databases for efficient data storage and retrieval, allowing seamless management and monitoring of the server network.
-
-#### [Dirt Launcher](https://github.com/d4julian)
-Created a custom modded Minecraft launcher using JavaFX. This launcher provided users with an intuitive interface for managing game mods and updates, enhancing the overall user experience.
-    `;
+####[Dirt Launcher](https://github.com/Dirt-Craft/Dirt-Launcher)
+Custom modded Minecraft launcher using JavaFX. This launcher provided users with an intuitive interface for managing game mods and updates, enhancing the overall user experience.
+####[Dirt Bot](https://github.com/Dirt-Craft/DirtBot)
+Fully modular Discord bot powered by JDA. This powers the ticket system, real-time tracking on servers, music, and various moderation commands.
+####[Stock Market Visualizer](https://github.com/d4julian/Stock-Market-Visualizer)
+The Stock Market Visualizer is designed to provide a dynamic view of stock market trends, focusing on top gainers, top losers, and most actively traded stocks.
+<br><br>
+*View more projects on my [GitHub](https://github.com/d4julian)*`;
 
 const java = new Date(2019, 3, 1);
 const web = new Date(2019, 10, 1);
